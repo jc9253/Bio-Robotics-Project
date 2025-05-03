cbed82a9369f8f1ec2fd535ce1d4ec258f3ec9df
HairySeldon <135975597+HairySeldon@users.noreply.github.com>
2025-05-03 09:43:39 -0400

    Refractured top level, added while loop

--- inference.py
+++ inference.py
@@ -1,48 +1,51 @@
 import time
-from zoom_ctrl import zoom_interface
 from data_collection.data_collection_script import Video 
 # from pupilextraction import ?
-
-os_zoom = zoom_interface()
-
-
+# from location_model.?
+# from zoom_model.?
+from zoom_ctrl import zoom_interface
 
 # init objects:
 camera = Video(path="person_0")
-
 ''' ####testing zoom control###
-pre_processing = ?()
+feature_extrac = ?()
 
 loc_model = ?()
 zoom_model = ?()
 
 ''' ####testing zoom control###
+os_zoom = zoom_interface()
+
 
 ## MAIN LOOP ##
+try: 
+    while True:
+       
+        # Take input data
+        recording = camera.start("") #unlabeled since inference
+        time.sleep(2)
+        camera.stop()
 
-# Take input data
-recording = camera.start("") #unlabeled since inference
-time.sleep(2)
-camera.stop()
+        ''' ####testing zoom control###
+        # Run through preprocessing
+        # Returns features which is a time series of:
+        # - pupil size
+        # -
+        # - 
 
-''' ####testing zoom control###
-# Run through preprocessing
-# Returns features which is a time series of:
-# - pupil size
-# -
-# - 
+        features = feature_extrac.?(recording)
 
-features = pre_processing.?(recording)
+        # Send features to zoom and location models
+        loc = loc_model.infer(features)
+        mag = zoom_model.infer(features)
 
-# Send to zoom and location models
-loc = loc_model.infer(features)
-mag = zoom_model.infer(features)
+        ''' ####testing zoom control###
 
-''' ####testing zoom control###
+        loc = [0.5, 0.5]
+        mag = 0
 
-loc = [0.5, 0.5]
-mag = 0
+        # Send location and zooom to zoom_interface
+        os_zoom.zoom(loc=loc, mag=mag)
 
-# Send location and zooom to zoom_interface
-
-os_zoom.zoom(loc=loc, mag=mag)
+except KeyboardInterrupt:
+    print("Complete")
