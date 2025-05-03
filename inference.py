8dd02c0da4c382233c1e69ebab0898e81409e868
HairySeldon <135975597+HairySeldon@users.noreply.github.com>
2025-05-03 11:01:04 -0400

    Requires feature extraction, location model, and zoom model

--- inference.py
+++ inference.py
@@ -1,6 +1,7 @@
 import time
-from data_collection.data_collection_script import Video 
+from data_collection.data_collection_script import Video
 from PupilExtraction.avitobmp import convert_avi
+
 # from location_model.?
 # from zoom_model.?
 from zoom_ctrl import zoom_interface
@@ -9,32 +10,32 @@ from zoom_ctrl import zoom_interface
 avi_path = "video"
 bmap_folder = "bmap/"
 camera = Video(path=avi_path, do_thread=False)
-''' ####testing zoom control###
+""" ####testing zoom control###
 feature_extrac = ?()
 
 loc_model = ?()
 zoom_model = ?()
 
-''' ####testing zoom control###
+"""  ####testing zoom control###
 os_zoom = zoom_interface()
 
 
 ## MAIN LOOP ##
-try: 
+try:
     while True:
-       
+
         # Take input data
-        recording = camera.start("") #unlabeled since inference
+        recording = camera.start("")  # unlabeled since inference
         time.sleep(2)
         camera.stop()
 
-        convert_avi("", bmap_folder) #convert avi to bit map
+        convert_avi("", bmap_folder)  # convert avi to bit map
 
-        ''' ####testing zoom control###
+        """ ####testing zoom control###
         # Run through preprocessing
         # Returns features which is a time series of:
+        # - time
         # - pupil size
-        # -
         # - 
 
         features = feature_extrac.?(recording)
@@ -42,8 +43,7 @@ try:
         # Send features to zoom and location models
         loc = loc_model.infer(features)
         mag = zoom_model.infer(features)
-
-        ''' ####testing zoom control###
+        """  ####testing zoom control###
 
         loc = [0.5, 0.5]
         mag = 0
